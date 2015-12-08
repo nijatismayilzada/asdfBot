267af8ff1444da0491877e9709946ffdde335264
Nijat Ismayilzada <nicat.ismayilzade@gmail.com>
2015-12-08 01:19:02 +0000

    asdfghjjhgfd

--- src/MKAgent/AsdfBot.java
+++ src/MKAgent/AsdfBot.java
@@ -3,6 +3,13 @@ package MKAgent;
 import java.io.IOException;
 
 public class AsdfBot {
+	
+  private Kalah kalah;
+  protected Side ourSide;
+  
+  public AsdfBot(int holes, int seeds){
+    this.kalah = new Kalah(new Board(holes, seeds));
+  }
 
   public void doMagic() {
 
@@ -20,17 +27,27 @@ public class AsdfBot {
               System.err.println("A start...");
               boolean first = Protocol.interpretStartMsg(s);
               System.err.println("Starting player? " + first);
-              Main.sendMsg(Protocol.createMoveMsg(1));
+              if(first){
+                ourSide = Side.SOUTH;
+                Main.sendMsg(Protocol.createMoveMsg(1));
+                Board moveBoard = new Board(this.kalah.getBoard());
+                Kalah.makeMove((Board)moveBoard, (Move)new Move(ourSide, 1));
+              }
+              else{
+                ourSide = Side.NORTH;
+                Main.sendMsg(Protocol.createSwapMsg());
+                ourSide = Side.SOUTH;
+              }
+              
               break;
 
             case STATE:
               System.err.println("A state...");
-              Board b = new Board(7, 7);
-              Protocol.MoveTurn r = Protocol.interpretStateMsg(s, b);
+              Protocol.MoveTurn r = Protocol.interpretStateMsg(s, kalah.getBoard());
               System.err.println("This was the move: " + r.move);
               System.err.println("Is the game over?: " + r.end);
               if (!r.end) System.err.println("Is it our turn again? " + r.again);
-              System.err.println("The board:\n" + b);
+              System.err.println("The board:\n" + kalah.getBoard());
               break;
 
             case END:
--- src/MKAgent/Main.java
+++ src/MKAgent/Main.java
@@ -55,7 +55,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		try {
-          AsdfBot asdf = new AsdfBot();
+          AsdfBot asdf = new AsdfBot(7, 7);
           asdf.doMagic();
         }
         catch(Exception e) {
