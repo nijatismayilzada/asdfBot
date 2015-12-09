eac10d4eaafebc497f2cc468ab8e187f76fb389a
Nijat Ismayilzada <nicat.ismayilzade@gmail.com>
2015-12-09 00:38:48 +0000

    redirecting system err output to /tmp/asdfBot.log file.

--- src/MKAgent/Main.java
+++ src/MKAgent/Main.java
@@ -1,8 +1,12 @@
 package MKAgent;
 import java.io.BufferedReader;
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.io.Reader;
 
 /**
@@ -54,8 +58,10 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		try {
-          AsdfBot asdf = new AsdfBot(7, 7);
+	  try {
+	      System.setErr(new PrintStream(new FileOutputStream(new File("/tmp/asdfBot.log"), true)));
+
+	      AsdfBot asdf = new AsdfBot(7, 7);
           asdf.doMagic();
         }
         catch(Exception e) {
