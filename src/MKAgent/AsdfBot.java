8d9d9cc50f0e9770723b78e5553f38ebc59f2706
korujzade <oruczade.kamil@gmail.com>
2015-12-10 04:26:34 +0000

    very basic heuristics

--- src/MKAgent/AsdfBot.java
+++ src/MKAgent/AsdfBot.java
@@ -6,6 +6,7 @@ public class AsdfBot {
 	
   private Kalah kalah;
   protected Side ourSide = Side.SOUTH;
+
   
   public AsdfBot(int holes, int seeds){
     this.kalah = new Kalah(new Board(holes, seeds));
@@ -18,6 +19,7 @@ public class AsdfBot {
       while (true) {
         System.err.println();
         s = Main.recvMsg();
+        boolean canSwap = false;
 
         System.err.println("Received: " + s);
         try {
@@ -25,6 +27,7 @@ public class AsdfBot {
           switch (mt) {
             case START:
               System.err.println("A start...");
+              // if true, our bot starts first
               boolean first = Protocol.interpretStartMsg(s);
               System.err.println("Starting player? " + first);
               if(first){
@@ -33,18 +36,21 @@ public class AsdfBot {
                 Board moveBoard = new Board(this.kalah.getBoard());
                 Kalah.makeMove((Board)moveBoard, (Move)new Move(ourSide, 1));
               }
-              else{
+              else {
                 ourSide = Side.NORTH;
+                canSwap = true;
                 Main.sendMsg(Protocol.createSwapMsg());
                 ourSide = Side.SOUTH;
               }
-              
-              break;
 
+              break;
             case STATE:
               
               System.err.println("A state...");
               Protocol.MoveTurn r = Protocol.interpretStateMsg(s, kalah.getBoard());
+              if (r.move == -1) {
+                swap();
+              }
               Board thisBoard = new Board(this.kalah.getBoard());
               for (int i=1; i<8;i++){
                 if(Kalah.isLegalMove(thisBoard, (Move) new Move(ourSide, i))){
@@ -67,10 +73,27 @@ public class AsdfBot {
         }
 
       }
-
     } catch (IOException e) {
       System.err.println("IOException: " + e.getMessage());
     }
   }
+
+  private void swap() {
+    ourSide = ourSide.opposite();
+  }
+
+  private int heuristics(Board board) {
+    int ourSeeds = board.getSeedsInStore(ourSide);
+    int oppSeeds = board.getSeedsInStore(ourSide.opposite());
+
+    for (int i = 1; i <= 7; i++) {
+      ourSeeds += board.getSeeds(ourSide, i);
+      oppSeeds += board.getSeeds(ourSide.opposite(), i);
+    }
+    int diff = ourSeeds - oppSeeds;
+    return diff;
+  }
+
+
 
 }
